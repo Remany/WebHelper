20f21f04fd8f1df9b9fe4ad08ebf2a4f2f169bf8
Basimov Rustam <sombra1nf@yandex.ru>
2023-05-19 19:20:38 +0500

    Init project

---
+++ src/main/java/ru/CatsProgers/WebHelper/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ru.CatsProgers.WebHelper;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WebHelperApplication.class);
+	}
+
+}
---
+++ src/main/java/ru/CatsProgers/WebHelper/WebHelperApplication.java
@@ -0,0 +1,13 @@
+package ru.CatsProgers.WebHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebHelperApplication.class, args);
+	}
+
+}
