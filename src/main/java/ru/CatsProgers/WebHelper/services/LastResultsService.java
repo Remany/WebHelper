27afbc09a657311c9b477e96047466e5f90300fa
Romanov Leonid <romanovleonid82@gmail.com>
2023-05-28 15:17:55 +1000

    fix bag in LastResultsService

--- src/main/java/ru/CatsProgers/WebHelper/services/LastResultsService.java
+++ src/main/java/ru/CatsProgers/WebHelper/services/LastResultsService.java
@@ -4,17 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.CatsProgers.WebHelper.models.AnalysisResult;
+import ru.CatsProgers.WebHelper.models.LastResult;
+import ru.CatsProgers.WebHelper.repositories.LastResultRepository;
 
 @Service
 @Transactional(readOnly = true)
 public class LastResultsService {
-    private final LastResultsRepository lastResultsRepository;
+    private final LastResultRepository lastResultRepository;
     @Autowired
-    public LastResultsService(LastResultsRepository lastResultsRepository) {
-        this.lastResultsRepository = lastResultsRepository;
+    public LastResultsService(LastResultRepository lastResultRepository) {
+        this.lastResultRepository = lastResultRepository;
     }
     @Transactional
-    public void addResult(AnalysisResult analysisResult){
-        lastResultsRepository.save(analysisResult);
+    public void addResult(LastResult lastResult){
+        lastResultRepository.save(lastResult);
     }
 }
